65ea60656398b6c1afdde4cb38df574f8c1a0d3a
Doug Ryan <ryan2135@pacificu.edu>
2022-07-20 15:26:18 -0700

    Initial Lab01

---
+++ src/Lab01THello.java
@@ -0,0 +1,6 @@
+public class Lab01THello {
+  public static void main (String[] args) {
+    System.out.println ("Hello GitHub");
+    System.out.println ("You successfully cloned the repository");
+  }
+}
