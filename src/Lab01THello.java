df00eaea4fce555e7008d75daefdba9da77c4356
Doug Ryan <ryan2135@pacificu.edu>
2022-07-20 15:50:16 -0700

    Added file header documentation

--- src/Lab01THello.java
+++ src/Lab01THello.java
@@ -1,3 +1,12 @@
+/*
+Filename:   Lab01Hello.java
+Author:     DR
+Course:     Java I
+Assignment: Lab01Hello
+Purpose:    Create a GitHub account, clone a repository, make a few changes
+            and push changes back to GitHub using GitHub Classroom
+Hours:      .1
+ */
 public class Lab01THello {
   public static void main (String[] args) {
     System.out.println ("Hello GitHub");
